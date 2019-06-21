a90235d9ca7b0fedc8b1ba238622ffd024034c2a
Cleverton Hoffmann <clevertonhoffmann@gmail.com>
2019-06-21 15:32:47 -0300

    Pesquisa1

---
+++ JavaConverter/src/Conversor/Carro.java
@@ -0,0 +1,72 @@
+package Conversor;
+
+/**
+ *
+ * @author Cleverton
+ */
+public class Carro {
+    
+    private String nome;
+    private int ano;
+    private String modelo;
+    private String marca;
+    private String combustivel;
+    private Double valor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(String combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" + "nome=" + nome + ", ano=" + ano + ", modelo=" + modelo + ", marca=" + marca + ", combustivel=" + combustivel + ", valor=" + valor + '}';
+    }
+    
+    public void text(){
+        String real = "10reais";
+    }
+}
--- JavaConverter/src/Conversor/Main.java
+++ JavaConverter/src/Conversor/Main.java
@@ -1,12 +1,8 @@
 package Conversor;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -19,9 +15,12 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         
+        Carro c = new Carro();
+        ConvertePHP t = new ConvertePHP(c);
+       // t.cria();
+                     
+        t.imprimeAtributoseValoresPojo(c); 
         
-        Teste t = new Teste();
-        t.cria();
     }
     
 }
