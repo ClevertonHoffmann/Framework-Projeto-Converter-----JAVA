33d404cfb37074dd0b06824bf386fb5f372c902c
Cleverton Hoffmann <clevertonhoffmann@gmail.com>
2019-07-04 11:33:15 -0300

    Finalizacao1

--- JavaConverter/src/Conversor/View/Main.java
+++ JavaConverter/src/Conversor/View/Main.java
@@ -5,6 +5,7 @@ import Conversor.ClassesEstruturais.EstruturaJavaScript;
 import Conversor.ClassesTestes.Carro;
 import Conversor.ClassesEstruturais.EstruturaPHP;
 import Conversor.ClassesEstruturais.EstruturaPython;
+import Conversor.ClassesTestes.Pessoa;
 import Conversor.Controller.Principal;
 
 /**
@@ -19,11 +20,12 @@ public class Main {
     public static void main(String[] args) {
         
         Carro c = new Carro();
-        
+       // Pessoa pes = new Pessoa();
+       // Principal p = new Principal(pes, new EstruturaPython(), ".py");
         Principal p = new Principal(c, new EstruturaPython(), ".py");
-        p = new Principal(c, new EstruturaPHP(), ".php");
-        p = new Principal(c, new EstruturaJavaScript(), ".js");      
-        p = new Principal(c, new EstruturaCmaismais(), ".cpp");
+      //  p = new Principal(c, new EstruturaPHP(), ".php");
+      //  p = new Principal(c, new EstruturaJavaScript(), ".js");      
+      //  p = new Principal(c, new EstruturaCmaismais(), ".cpp");
                 
     }
     
